48a1c38c1c0356cfc1250fdeca4fb8c045c54648
April Skrine <april.skrine@gmail.com>
2022-03-16 10:03:35 -0500

    commit tree 6ac54c3c63451016ffee6e64584622289735ad8a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    def is_clickbait
+        validates :book_name, inclusion: { in: %w("Won't Believe" "Secret" "Top" "Guess") }
+    end
+
 end
